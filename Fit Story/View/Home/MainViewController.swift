f26e2b40e4691353100287a60223554d0f04c3d6
Han <s3876531@gmail.com>
2022-04-09 15:40:50 +0800

    initial commit

---
+++ Fit Story/View/Home/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  Fit Story
+//
+//  Created by 陳翰霖 on 2022/4/9.
+//
+
+import UIKit
+
+class MainViewController:UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+  }
+}
